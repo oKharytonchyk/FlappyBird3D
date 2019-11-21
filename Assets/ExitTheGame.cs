f6bc12180e5c6d8fbad8cd9f8c1e0728d8efe4f3
oKharytonchyk <ledoff.sky@ukr.net>
2019-11-21 13:24:44 +0200

    demo

---
+++ Assets/ExitTheGame.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitTheGame : MonoBehaviour
+{
+
+    public Button exitButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitButton.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TaskOnClick() {
+        Application.Quit();
+    }
+}
